be55a9b13489ae80f02cf76f670e31aba3b16610
Abdurehman Kero <keroabdurehman@gmail.com>
2025-11-17 19:21:38 +0300

    ENV  file updated

--- src/component/YoutubeVideos/YoutubeVideos.jsx
+++ src/component/YoutubeVideos/YoutubeVideos.jsx
@@ -3,11 +3,12 @@ import { useEffect, useState } from "react";
 const YoutubeVideos = () => {
   const [appleVideos, setAppleVideos] = useState([]);
 
+  const apiKey = import.meta.env.VITE_API_KEY;
   useEffect(() => {
     const getAppleVideos = async () => {
       try {
         const response = await fetch(
-          `https://www.googleapis.com/youtube/v3/search?key=AIzaSyA1T8kDTy54k-UGGupth3sGrqdTNt2l7Po&channelId=UCE_M8A5yxnLfW0KghEeajjw&part=snippet&order=date&maxResults=9&type=video`
+          `https://www.googleapis.com/youtube/v3/search?key= ${apiKey}&channelId=UCE_M8A5yxnLfW0KghEeajjw&part=snippet&order=date&maxResults=9&type=video`
         );
         const data = await response.json();
         setAppleVideos(data?.items);
